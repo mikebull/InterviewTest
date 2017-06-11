34a497394d3a51e62f45c03ab58f0910de115b67
mikebull <hello@mikebull.info>
2017-06-11 23:32:37 +0100

    Reinstall NuGet packages to fix issue, and update DI mapping

--- FundsLibrary.InterviewTest.Web/Installers/RepositoriesInstaller.cs
+++ FundsLibrary.InterviewTest.Web/Installers/RepositoriesInstaller.cs
@@ -2,6 +2,7 @@
 using Castle.MicroKernel.SubSystems.Configuration;
 using Castle.Windsor;
 using FundsLibrary.InterviewTest.Web.Repositories;
+using System.Configuration;
 
 namespace FundsLibrary.InterviewTest.Web.Installers
 {
@@ -9,9 +10,16 @@ namespace FundsLibrary.InterviewTest.Web.Installers
     {
         public void Install(IWindsorContainer container, IConfigurationStore store)
         {
+            var serviceAppUrl = ConfigurationManager.AppSettings["fundsLibraryUrl"];
+            var authToken = ConfigurationManager.AppSettings["authToken"];
+
             container.Register(Classes.FromThisAssembly()
                 .Pick().If(t => t.Name.EndsWith("Repository"))
                 .WithService.AllInterfaces());
+
+            container.Register(Component
+                .For<IODataClientWrapper>()
+                .UsingFactoryMethod(_ => new ODataClientWrapper(serviceAppUrl, authToken)));
 
             container.Register(Component
                 .For<IHttpClientWrapper>()
