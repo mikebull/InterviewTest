1caba921fc8ff0b7f16e81aaeb846aba550f1f4e
mikebull <hello@mikebull.info>
2017-06-11 22:41:30 +0100

    Add repository and odata wrapper

---
+++ FundsLibrary.InterviewTest.Web/Repositories/ODataClientWrapper.cs
@@ -0,0 +1,31 @@
+﻿using FundsLibrary.InterviewTest.Common.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace FundsLibrary.InterviewTest.Web.Repositories
+{
+    public interface IODataClientWrapper
+    {
+        Task<IEnumerable<Security>> GetFundsForManager(Guid managerGuid);
+    }
+
+    public class ODataClientWrapper : IODataClientWrapper
+    {
+        private readonly string _serviceAppUrl;
+        private readonly string _authToken;
+
+        public ODataClientWrapper(string serviceAppUrl, string authToken)
+        {
+            _serviceAppUrl = serviceAppUrl;
+            _authToken = authToken;
+        }
+
+        public Task<IEnumerable<Security>> GetFundsForManager(Guid managerGuid)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ FundsLibrary.InterviewTest.Web/Repositories/SecuritiesRepository.cs
@@ -0,0 +1,29 @@
+﻿using FundsLibrary.InterviewTest.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace FundsLibrary.InterviewTest.Web.Repositories
+{
+    public interface ISecuritiesRepository
+    {
+        Task<IEnumerable<Fund>> GetFunds(Guid managerGuid);
+    }
+
+    public class SecuritiesRepository : ISecuritiesRepository
+    {
+        private readonly IODataClientWrapper _client;
+
+        public SecuritiesRepository(IODataClientWrapper client)
+        {
+            _client = client;
+        }
+
+        public Task<IEnumerable<Fund>> GetFunds(Guid managerGuid)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
