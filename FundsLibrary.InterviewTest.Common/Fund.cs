03cdbc0511ead316900f05d6c78476c1de91a12d
mikebull <hello@mikebull.info>
2017-06-11 22:33:00 +0100

    Add models

---
+++ FundsLibrary.InterviewTest.Common/Fund.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FundsLibrary.InterviewTest.Common
+{
+    public class Fund
+    {
+        [Display(Name = "IA Sector")]
+        public string IASector { get; set; }
+        [Display(Name = "Benchmark Description")]
+        public string BenchmarkDescription { get; set; }
+        [Display(Name = "ISIN Code")]
+        public string IsinCode { get; set; }
+        public string Objectives { get; set; }
+        public string FullName { get; set; }
+    }
+}
